e943e341dd2c5d39b3f75d690502d71448a05cc6
bruce-wayne <2231593226@qq.com>
2023-01-01 23:35:58 +0800

    add mock

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
+import { UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import vueJSX from '@vitejs/plugin-vue-jsx'
+import { resolve } from 'path'
+import windicss from 'vite-plugin-windicss'
+import { viteMockServe } from 'vite-plugin-mock'
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default (): UserConfigExport => {
+    return {
+        plugins: [vue(), vueJSX(), windicss(), viteMockServe()],
+        resolve: {
+            alias: {
+                '@': resolve(__dirname, 'src'),
+            },
+        },
+    }
+}
